6de1bb90c2c8fcb0677dc78155f12442b73adeb5
Sumonta <amitnandy112@gmail.com>
2023-08-13 13:26:08 +0600

    frontend templete added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+//import { ProSidebarProvider } from 'react-pro-sidebar';
+//import SidebarMenu from './components/Sidebar';
+import MiniDrawer from './components/NewBar';
+
+import { StyledEngineProvider } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
+import AppRoutes from './components/AppRoutes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StyledEngineProvider injectFirst>
+    <div>
+        <MiniDrawer />
+        <AppRoutes />
+    </div>
+    </StyledEngineProvider>
   </React.StrictMode>
 );
 
