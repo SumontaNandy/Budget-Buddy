18acc025a230e4acd2e03bcf7ec50b9d1b3e8943
Farhan Tanvir <1805073@ugrad.cse.buet.ac.bd>
2023-08-30 17:13:52 +0600

    Income part of transaction fixed

--- frontend/src/data/TransactionData.js
+++ frontend/src/data/TransactionData.js
@@ -1,12 +1,12 @@
 const TransactionData = [
     {
-      date: "2023-08-20 18:45:42.361539+06",
+      date: "2023-09-10 18:45:42.361539+06",
       category: "Grocery",
       amount: 500,
       account: "ICCU Checking"
     },
     {
-        date: "2023-08-25 18:45:42.361539+06",
+        date: "2023-09-25 18:45:42.361539+06",
         category: "Paycheck",
         amount: 750,
         account: "Walmart Card"
